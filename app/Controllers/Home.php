2b06d77d3e26ab955e640e97dfaef76f12174d28
Naufal Hilal <92293302+NaufallHilal@users.noreply.github.com>
2023-09-11 19:49:15 +0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,23 +9,4 @@ class Home extends BaseController
         return view('welcome_message');
     }
 
-    public function profile($nama="",$npm="",$kelas=""): string
-    {
-        $data=[
-            'nama'=>$nama,
-            'npm'=>$npm,
-            'kelas'=>$kelas,
-        ];
-
-        return view('profile',$data);
-    }
-}
-
-class Profile extends BaseController
-{
-
-    public function profile(): string
-    {
-        return view('welcome_message');
-    }
 }
