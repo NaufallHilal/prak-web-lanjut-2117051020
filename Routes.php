7855f19601beb92811eb54f6d8c8e7b033b33ac0
Naufal Hilal <92293302+NaufallHilal@users.noreply.github.com>
2023-09-11 19:40:20 +0700

    Add files via upload

---
+++ Routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile/$1/$2/$3');
+$routes->get('/profile', 'Profile::profile');
+
